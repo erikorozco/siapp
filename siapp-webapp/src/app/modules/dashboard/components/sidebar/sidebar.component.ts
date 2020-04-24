7b98dc8cb36515c10fe8efa71d33b586c9b1421e
erikorozco <sesenta28@hotmail.com>
2020-04-24 11:57:37 -0500

    Toggle menu, scroll tables

--- siapp-webapp/src/app/modules/dashboard/components/sidebar/sidebar.component.ts
+++ siapp-webapp/src/app/modules/dashboard/components/sidebar/sidebar.component.ts
@@ -12,14 +12,21 @@ export class SidebarComponent implements OnInit {
   @Input() userDetails: any;
   isAdmin;
   isAdministrative;
+  isSidebarToggled;
 
   constructor(
     private authService: AuthService
-  ) { }
+  ) {
+    this.isSidebarToggled = false;
+  }
 
   ngOnInit() {
     this.isAdministrative = this.authService.isAllowed(this.userDetails.roles, this.authService.RECEPTION_MODULE);
     this.isAdmin = this.authService.isAllowed(this.userDetails.roles, this.authService.ADMIN_MODULES);
+  }
+
+  toggleSidebar(isSidebarToggled) {
+    this.isSidebarToggled = !isSidebarToggled;
   }
 
 }
